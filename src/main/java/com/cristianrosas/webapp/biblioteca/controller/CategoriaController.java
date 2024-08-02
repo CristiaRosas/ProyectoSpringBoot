e112ab25f14c61739f06105c127db2493dd6e51a
Informatica <informatica@alumno.local>
2024-08-02 16:42:22 -0600

    Crud Categoria

--- src/main/java/com/cristianrosas/webapp/biblioteca/controller/CategoriaController.java
+++ src/main/java/com/cristianrosas/webapp/biblioteca/controller/CategoriaController.java
@@ -6,8 +6,11 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +26,18 @@ public class CategoriaController {
     @Autowired
     CategoriaService categoriaService;
 
-    @GetMapping("/{id}")
+    @GetMapping("/")
     public List<Categoria> listaCategorias(){
         return categoriaService.listarCategorias();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> buscarCategoriaPorId(@PathVariable Long id){
+        try{
+           return ResponseEntity.ok(categoriaService.buscarCategoriaPorId(id));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
     @PostMapping("/")
@@ -42,5 +54,35 @@ public class CategoriaController {
 
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> editarCategoria(@PathVariable Long id, @RequestBody Categoria categoriaNueva){
+        Map<String, String> response = new HashMap<>();
+        try {
+            Categoria categoria = categoriaService.buscarCategoriaPorId(id);
+            categoria.setNombreCategoria(categoriaNueva.getNombreCategoria());
+            categoriaService.guardarCategoria(categoria);
+            response.put("message", "La Categoria se ha editado con exito");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("message", "La categoria no se pudo editar");
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+       
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> eliminarCategoria(@PathVariable Long id){
+        Map<String, String> response = new HashMap<>();
+        try{
+            Categoria categoria = categoriaService.buscarCategoriaPorId(id);
+            categoriaService.eliminarCategoria(categoria);
+            response.put("message", "Categoria eliminada con exito");
+            return ResponseEntity.ok(response);
+        }catch(Exception e){
+            response.put("message", "La categoria no se pudo eliminar");
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
 
 }
